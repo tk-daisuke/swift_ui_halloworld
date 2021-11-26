cb0887ff66b1c0df3760b67cb3303b2a86f72713
daisuke <grass536@gmail.com>
2021-11-26 15:36:58 +0900

    Initial Commit

---
+++ first_project/first_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_projectApp.swift
+//  first_project
+//
+//  Created by 佐藤 大輔 on 2021/11/26.
+//
+
+import SwiftUI
+
+@main
+struct first_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
