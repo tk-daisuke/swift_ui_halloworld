88e357a7b038f4b1424ee196771f05c6bf4a419d
daisuke <grass536@gmail.com>
2021-12-19 20:48:18 +0900

    update view

--- first_project/ContentView.swift
+++ first_project/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack() {
+            Image("flutter_logo").resizable()
+                .frame(width: 44.0, height: 44.0)
+                
+            VStack(alignment: .leading) {
+                Text("sugoi")
+                    .font(.headline)
+                    .fontWeight(.light)
+                    .foregroundColor(.black)
+                Text("Flutter")
+                    .font(.title)
+                    .fontWeight(.medium)
+                    .foregroundColor(.black)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
