301559244f9b75f174bede4e76d0487d85dd2929
daisuke <grass536@gmail.com>
2021-12-20 19:15:33 +0900

    add mocks

---
+++ first_project/Preview Content/User+Mock.swift
@@ -0,0 +1,15 @@
+//
+//  User+Mock.swift
+//  first_project
+//
+//  Created by 佐藤 大輔 on 2021/12/20.
+//
+
+import Foundation
+extension User {
+    static let mock1 = User(name: "Test User1")
+    static let mock2 = User(name: "Test User2")
+    static let mock3 = User(name: "Test User3")
+    static let mock4 = User(name: "Test User4")
+    static let mock5 = User(name: "Test User5")
+}
